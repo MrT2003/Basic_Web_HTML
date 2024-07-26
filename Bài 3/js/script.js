14cd7cb6e01eab6a5a93dc0b0b1203f4676f0cc1
MrT2008 <haitrieu6212@gmail.com>
2024-07-26 11:04:19 +0700

    done number (win)

--- Bài 3/js/script.js
+++ Bài 3/js/script.js
@@ -1,14 +1,16 @@
-$(document).ready(function(){
-    $('.slick_content').slick({
-        dots: true,
-        infinite: true,
-        speed: 1100,
-        slidesToShow: 5,
-        autoplay: true,
-        autoplaySpeed: 4000,
-    });
-});
+$(document).ready(function () {
+//   $(".slick_content").slick({
+//     dots: true,
+//     infinite: true,
+//     speed: 1100,
+//     slidesToShow: 5,
+//     autoplay: true,
+//     autoplaySpeed: 4000,
+//   });
 
-$('[data-fancybox="Gallery"]').fancybox({
-    loop: true
+//   $('[data-fancybox="Gallery"]').fancybox({
+//     loop: true,
+//   });
+
+  $('.counter').countUp();
 });
