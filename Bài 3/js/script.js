a3e91db8ea0301715ed1873113b175459b8d5121
MrT2008 <haitrieu6212@gmail.com>
2024-07-26 16:55:56 +0700

    fix service

--- Bài 3/js/script.js
+++ Bài 3/js/script.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
-//   $(".slick_content").slick({
-//     dots: true,
-//     infinite: true,
-//     speed: 1100,
-//     slidesToShow: 5,
-//     autoplay: true,
-//     autoplaySpeed: 4000,
-//   });
+  $(".slick_content").slick({
+    dots: true,
+    infinite: true,
+    speed: 1100,
+    slidesToShow: 5,
+    autoplay: true,
+    autoplaySpeed: 4000,
+  });
 
 //   $('[data-fancybox="Gallery"]').fancybox({
 //     loop: true,
