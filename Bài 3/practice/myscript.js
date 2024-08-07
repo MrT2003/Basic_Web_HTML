c327a49b67adafb4c7532cba68b3803642b54208
MrT2008 <haitrieu6212@gmail.com>
2024-08-07 11:06:53 +0700

    add service image

---
+++ Bài 3/practice/myscript.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $(".slick_container").slick({
+      dots: true,
+      infinite: true,
+      speed: 1100,
+      slidesToShow: 5,
+      autoplay: true,
+      autoplaySpeed: 4000,
+    });
+  });
+  
